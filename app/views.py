0f30c14b42fdea49fb3b4f5e4c648ec7f04b9d8e
ASANO HIRO <hiroasano1120@icloud.com>
2024-11-14 11:08:10 +0900

    マップ表示できたよ

--- app/views.py
+++ app/views.py
@@ -23,13 +23,11 @@ def index(request):
 def map(request):
   return render(request, 'app/map.html')
 
-
 def return_upload_image(request):
     return render(request, 'app/upload.html')
 
 def warning_page(request):
     render(request,'warning.html')
-
 
 def resize_image(image, max_size=(400, 400)):
     # Open the image
@@ -48,7 +46,6 @@ def resize_image(image, max_size=(400, 400)):
     img_byte_arr = img_byte_arr.getvalue()
 
     return img_byte_arr
-
 
 # AWSからのラベルを検出する関数
 def detect_labels_in_image(image):
@@ -69,7 +66,7 @@ def detect_labels_in_image(image):
 
 # ラベルマッピング辞書
 label_mapping = {
-    'Accessories': '付属品',
+    'Accessories': 'アクセサリー',
     'Animal': '動物',
     'Bag': 'バック',
     'Binoculars': '双眼鏡',
@@ -121,37 +118,140 @@ label_mapping = {
 INAPPROPRIATE_LABELS = {
     'Face'
 }
+# 47都道府県辞書
+prefecture_mapping = {
+    'Hokkaido': '北海道',
+    'Aomori': '青森県',
+    'Iwate': '岩手県',
+    'Miyagi': '宮城県',
+    'Akita': '秋田県',
+    'Yamagata': '山形県',
+    'Fukushima': '福島県',
+    'Ibaraki': '茨城県',
+    'Tochigi': '栃木県',
+    'Gunma': '群馬県',
+    'Saitama': '埼玉県',
+    'Chiba': '千葉県',
+    'Tokyo': '東京都',
+    'Kanagawa': '神奈川県',
+    'Niigata': '新潟県',
+    'Toyama': '富山県',
+    'Ishikawa': '石川県',
+    'Fukui': '福井県',
+    'Yamanashi': '山梨県',
+    'Nagano': '長野県',
+    'Gifu': '岐阜県',
+    'Shizuoka': '静岡県',
+    'Aichi': '愛知県',
+    'Mie': '三重県',
+    'Shiga': '滋賀県',
+    'Kyoto': '京都府',
+    'Osaka': '大阪府',
+    'Hyogo': '兵庫県',
+    'Nara': '奈良県',
+    'Wakayama': '和歌山県',
+    'Tottori': '鳥取県',
+    'Shimane': '島根県',
+    'Okayama': '岡山県',
+    'Hiroshima': '広島県',
+    'Yamaguchi': '山口県',
+    'Tokushima': '徳島県',
+    'Kagawa': '香川県',
+    'Ehime': '愛媛県',
+    'Kochi': '高知県',
+    'Fukuoka': '福岡県',
+    'Saga': '佐賀県',
+    'Nagasaki': '長崎県',
+    'Kumamoto': '熊本県',
+    'Oita': '大分県',
+    'Miyazaki': '宮崎県',
+    'Kagoshima': '鹿児島県',
+    'Okinawa': '沖縄県'
+}
 
 # ラベルのマッピングとフォームへの反映
 def extract_relevant_labels(labels):
-    # 対応するフォームフィールドの値を保存する辞書
-    form_data = {
-        'item_name': '',
-        'genre': '',
-        'color': ''
-    }
-
+    item_name = None
     for label in labels:
-        print(f"Detected label: {label['Name']}")
-
-        if label['Name'] == 'Credit Card':
-            form_data['item_name'] = label_mapping['Credit Card']
-            break  # 「クレジットカード」を見つけたら以降の処理をしない
-
-        elif label['Name'] == 'ID Card':
-            form_data['item_name'] = label_mapping['ID Card']
-
-        # 他のラベルの処理
+        if label['Name'] == 'Accessories':
+            item_name = label_mapping['Accessories']
+            break
+        elif label['Name'] == 'Animal':
+            item_name = label_mapping['Animal']
+            break
+        elif label['Name'] == 'Bag':
+            item_name = label_mapping['Bag']
+            break
+        elif label['Name'] == 'Binoculars':
+            item_name = label_mapping['Binoculars']
+            break
+        elif label['Name'] == 'Book':
+            item_name = label_mapping['Book']
+            break
+        elif label['Name'] == 'Camera':
+            item_name = label_mapping['Camera']
+            break
+        elif label['Name'] == 'Clothing':
+            item_name = label_mapping['Clothing']
+            break
+        elif label['Name'] == 'Computer Hardware':
+            item_name = label_mapping['Computer Hardware']
+            break
+        elif label['Name'] == 'Contact Lens':
+            item_name = label_mapping['Contact Lens']
+            break
+        elif label['Name'] == 'Cosmetics':
+            item_name = label_mapping['Cosmetics']
+            break
+        elif label['Name'] == 'Credit Card':
+            item_name = label_mapping['Credit Card']
+            break
+        elif label['Name'] == 'Diaper':
+            item_name = label_mapping['Diaper']
+            break
+        elif label['Name'] == 'Electrical Device':
+            item_name = label_mapping['Electrical Device']
+            break
         elif label['Name'] == 'Electronics':
-            form_data['genre'] = '電子機器'
-
-    # 'item_name' が空の場合はデフォルト値を設定
-    if not form_data['item_name']:
-        form_data['item_name'] = '不明'
-
-    print(f"Final form_data: {form_data}")
-    return form_data
-
+            item_name = label_mapping['Electronics']
+            break
+        elif label['Name'] == 'Envelope':
+            item_name = label_mapping['Envelope']
+            break
+        elif label['Name'] == 'Glasses':
+            item_name = label_mapping['Glasses']
+            break
+        elif label['Name'] == 'Glove':
+            item_name = label_mapping['Glove']
+            break
+        elif label['Name'] == 'Hat':
+            item_name = label_mapping['Hat']
+            break
+        elif label['Name'] == 'ID Card':
+            item_name = label_mapping['ID Card']
+            break
+        elif label['Name'] == 'Jewelry':
+            item_name = label_mapping['Jewelry']
+            break
+        elif label['Name'] == 'Key':
+            item_name = label_mapping['Key']
+            break
+        elif label['Name'] == 'Light':
+            item_name = label_mapping['Light']
+            break
+        elif label['Name'] == 'Lighter':
+            item_name = label_mapping['Lighter']
+            break
+        elif label['Name'] == 'Medication':
+            item_name = label_mapping['Medication']
+            break
+        elif label['Name'] == 'Mobile Phone':
+            item_name = label_mapping['Mobile Phone']
+            break
+        elif label['Name'] == 'Money':
+            item_name = label_mapping['Money']
+            break
+    return item_name
 
 def get_prefecture_from_location(latitude, longitude):
     api_key = settings.GOOGLE_MAPS_API_KEY
@@ -183,6 +283,106 @@ def get_prefecture_from_location(latitude, longitude):
 
     return "不明"
 
+def prefecture_change_japan(prefecture):
+    prefecture_name = None
+    if prefecture == 'Hokkaido':
+        prefecture_name = prefecture_mapping['Hokkaido']
+    elif prefecture == 'Aomori':
+        prefecture_name = prefecture_mapping['Aomori']
+    elif prefecture == 'Iwate':
+        prefecture_name = prefecture_mapping['Iwate']
+    elif prefecture == 'Miyagi':
+        prefecture_name = prefecture_mapping['Miyagi']
+    elif prefecture == 'Akita':
+        prefecture_name = prefecture_mapping['Akita']
+    elif prefecture == 'Yamagata':
+        prefecture_name = prefecture_mapping['Yamagata']
+    elif prefecture == 'Fukushima':
+        prefecture_name = prefecture_mapping['Fukushima']
+    elif prefecture == 'Ibaraki':
+        prefecture_name = prefecture_mapping['Ibaraki']
+    elif prefecture == 'Tochigi':
+        prefecture_name = prefecture_mapping['Tochigi']
+    elif prefecture == 'Gunma':
+        prefecture_name = prefecture_mapping['Gunma']
+    elif prefecture == 'Saitama':
+        prefecture_name = prefecture_mapping['Saitama']
+    elif prefecture == 'Chiba':
+        prefecture_name = prefecture_mapping['Chiba']
+    elif prefecture == 'Tokyo':
+        prefecture_name = prefecture_mapping['Tokyo']
+    elif prefecture == 'Kanagawa':
+        prefecture_name = prefecture_mapping['Kanagawa']
+    elif prefecture == 'Niigata':
+        prefecture_name = prefecture_mapping['Niigata']
+    elif prefecture == 'Toyama':
+        prefecture_name = prefecture_mapping['Toyama']
+    elif prefecture == 'Ishikawa':
+        prefecture_name = prefecture_mapping['Ishikawa']
+    elif prefecture == 'Fukui':
+        prefecture_name = prefecture_mapping['Fukui']
+    elif prefecture == 'Yamanashi':
+        prefecture_name = prefecture_mapping['Yamanashi']
+    elif prefecture == 'Nagano':
+        prefecture_name = prefecture_mapping['Nagano']
+    elif prefecture == 'Gifu':
+        prefecture_name = prefecture_mapping['Gifu']
+    elif prefecture == 'Shizuoka':
+        prefecture_name = prefecture_mapping['Shizuoka']
+    elif prefecture == 'Aichi':
+        prefecture_name = prefecture_mapping['Aichi']
+    elif prefecture == 'Mie':
+        prefecture_name = prefecture_mapping['Mie']
+    elif prefecture == 'Shiga':
+        prefecture_name = prefecture_mapping['Shiga']
+    elif prefecture == 'Kyoto':
+        prefecture_name = prefecture_mapping['Kyoto']
+    elif prefecture == 'Osaka':
+        prefecture_name = prefecture_mapping['Osaka']
+    elif prefecture == 'Hyogo':
+        prefecture_name = prefecture_mapping['Hyogo']
+    elif prefecture == 'Nara':
+        prefecture_name = prefecture_mapping['Nara']
+    elif prefecture == 'Wakayama':
+        prefecture_name = prefecture_mapping['Wakayama']
+    elif prefecture == 'Tottori':
+        prefecture_name = prefecture_mapping['Tottori']
+    elif prefecture == 'Shimane':
+        prefecture_name = prefecture_mapping['Shimane']
+    elif prefecture == 'Okayama':
+        prefecture_name = prefecture_mapping['Okayama']
+    elif prefecture == 'Hiroshima':
+        prefecture_name = prefecture_mapping['Hiroshima']
+    elif prefecture == 'Yamaguchi':
+        prefecture_name = prefecture_mapping['Yamaguchi']
+    elif prefecture == 'Tokushima':
+        prefecture_name = prefecture_mapping['Tokushima']
+    elif prefecture == 'Kagawa':
+        prefecture_name = prefecture_mapping['Kagawa']
+    elif prefecture == 'Ehime':
+        prefecture_name = prefecture_mapping['Ehime']
+    elif prefecture == 'Kochi':
+        prefecture_name = prefecture_mapping['Kochi']
+    elif prefecture == 'Fukuoka':
+        prefecture_name = prefecture_mapping['Fukuoka']
+    elif prefecture == 'Saga':
+        prefecture_name = prefecture_mapping['Saga']
+    elif prefecture == 'Nagasaki':
+        prefecture_name = prefecture_mapping['Nagasaki']
+    elif prefecture == 'Kumamoto':
+        prefecture_name = prefecture_mapping['Kumamoto']
+    elif prefecture == 'Oita':
+        prefecture_name = prefecture_mapping['Oita']
+    elif prefecture == 'Miyazaki':
+        prefecture_name = prefecture_mapping['Miyazaki']
+    elif prefecture == 'Kagoshima':
+        prefecture_name = prefecture_mapping['Kagoshima']
+    elif prefecture == 'Okinawa':
+        prefecture_name = prefecture_mapping['Okinawa']
+
+    return prefecture_name
+
+
 # 実際の検出と自動入力の実行
 # views.py
 s3 = boto3.client('s3',
@@ -204,11 +404,10 @@ def upload_image(request):
             image_bytes_for_upload = io.BytesIO(image_data)  # S3アップロード用のファイルオブジェクト
             image_bytes_for_resize = io.BytesIO(image_data)# リサイズ用のファイルオブジェクト
 
-            print(image_bytes_for_upload)
-            print(image_bytes_for_resize)
-
             # Get the prefecture from location data
             prefecture = get_prefecture_from_location(latitude, longitude)
+            prefecture_jp = prefecture_change_japan(prefecture)
+
 
             # Generate a unique filename
             file_extension = image.name.split('.')[-1]
@@ -241,7 +440,7 @@ def upload_image(request):
                 'item_name': end_label,
                 'latitude': latitude,
                 'longitude': longitude,
-                'prefecture': prefecture,
+                'prefecture': prefecture_jp,
                 'image_url': image_url,
                 'detected_labels': labels
             }
@@ -249,7 +448,6 @@ def upload_image(request):
             return render(request, 'app/upload_result.html', context)
 
     return render(request, 'app/upload.html')
-
 
 def upload_image_result(request):
     if request.method == 'POST':
@@ -271,7 +469,6 @@ def upload_image_result(request):
 
         return render(request, 'app/upload_completion.html')
 
-
 def map_view(request):
     if request.method == 'GET':
         google_maps_api_key = AWS.settings.env('GOOGLE_MAPS_API_KEY')
@@ -283,37 +480,41 @@ def map_view(request):
         }
         return render(request, 'app/map.html', context)
 
-
 # 全てのアイテムを削除
 def delete_all_items(request):
     LostItem.objects.all().delete()
     return redirect('map_view')
 
-
 def search_items(request):
-    item_name = request.GET.get('item_name', '')
-    prefecture = request.GET.get('prefecture', '')
+    try:
+        # クエリパラメータの取得
+        item_name = request.GET.get('item_name', '').strip()
+        prefecture = request.GET.get('prefecture', '').strip()
 
-    # 検索条件を元にデータをフィルタリング
-    items = LostItem.objects.all()
-    if item_name:
-        items = items.filter(description__icontains=item_name)
-    if prefecture:
-        items = items.filter(prefecture=prefecture)
+        # フィルタリング条件
+        items = LostItem.objects.all()
+        if item_name:
+            items = items.filter(description__icontains=item_name)
+        if prefecture:
+            items = items.filter(prefecture__icontains=prefecture)
 
-    # JSON形式でレスポンスを返す
-    items_data = []
-    for item in items:
-        items_data.append({
-            'fields': {
-                'image':item.image,
+        # 結果のシリアライズ
+        items_data = [
+            {
+                'id': item.id,
+                'date_time': item.date_time.strftime('%Y-%m-%d %H:%M:%S'),
                 'description': item.description,
-                'image_url': item.image.url if item.image else '',
+                'image_url': item.image_url if item.image_url else None,
                 'latitude': item.latitude,
                 'longitude': item.longitude,
-                'date_time':item.date_time,
+                'prefecture': item.prefecture,
             }
-        })
+            for item in items
+        ]
 
-    return JsonResponse(items_data, safe=False)
+        return JsonResponse(items_data, safe=False)
+    except Exception as e:
+        # ログの出力 (必要に応じて)
+        print(f"Error in search_items: {e}")
+        return JsonResponse({'error': str(e)}, status=500)
 
